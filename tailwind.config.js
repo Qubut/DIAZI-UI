dd9571210be9238f5fef7eb7eca57916e7ac8d54
Qubut <qubut@tutanota.com>
2023-08-10 00:06:25 +0200

    removed unnecessary pkg

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,9 @@
 module.exports = {
   content: [
     "./src/**/*.{html,ts,tsx,jsx,css}",
-    "./node_modules/tw-elements/dist/js/**/*.js",
   ],
 
   theme: {
     extend: {},
   },
-  plugins: [require("tw-elements/dist/plugin")]
 };
